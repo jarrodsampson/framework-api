a1d485aff9d4b33fb34965f522926ae7d6eb4979
Jarrod Sampson <jarrod.sampson91@gmail.com>
2015-10-15 15:55:30 -0700

    Create AccessClass.php

---
+++ AccessClass.php
@@ -0,0 +1,21 @@
+<?php
+
+class AccessObject
+{
+
+	public static $host;
+	public static $username;
+	public static $password;
+	public static $database;
+
+	public function __construct()
+	{
+	  $this->host = "xxxxxxxx";
+		$this->username = "xxxxxxxx";
+		$this->password = "xxxxxxxx";
+		$this->database = "xxxxxxxx";
+	}
+	
+}
+
+?>
